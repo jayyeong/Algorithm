fcce37148262a1a197922f6f1934eeae3c8f1694
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-02-25 01:33:41 +0900

    .

---
+++ BJproblem/0224_BOJ11279.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+N = int(input())
+box = []
+for _ in range(N):
+    x = int(sys.stdin.readline())
+
+    if x == 0:
+        if len(box) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(box)[1])
+    elif x > 0:
+        heapq.heappush(box,(-x,x))
