3c92f14e993a22577067830a72904901ae45f3db
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-03-07 19:09:32 +0900

    .

--- BJproblem/BOJ2579계단오르기.py
+++ BJproblem/BOJ2579계단오르기.py
@@ -4,9 +4,14 @@ stairs = []
 for _ in range(N):
     stairs.append(int(sys.stdin.readline()))
 
-count = 0
-DP = [0] * 9
-for i in range(N):
-    if count == 2:
-        DP[i] = max()
-    DP[i] = 1
+DP = []
+
+DP.append(stairs[0])
+if N >= 2:
+    DP.append(stairs[0] + stairs[1])
+if N >= 3:
+    DP.append(max(stairs[0] + stairs[2], stairs[1] + stairs[2]))
+for i in range(3,N):
+    DP.append(max(DP[i - 3] + stairs[i - 1] + stairs[i],DP[i - 2] + stairs[i]))
+
+print(DP[N - 1])
