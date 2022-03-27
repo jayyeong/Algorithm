e86a4f50b303b1484f177dbc78d43347846c1734
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-03-27 17:12:50 +0900

    .

---
+++ programmers/신고 결과 받기.py
@@ -0,0 +1,18 @@
+def solution(id_list, report, k):
+    # 신고받은 사람 기준으로 분류
+    r_num = [[] for x in range(len(id_list))]
+    # print(r_num)
+    for x in report:
+        y = x.split()
+        if not x in r_num[id_list.index(y[1])]:
+            r_num[id_list.index(y[1])].append(x)
+    print(r_num)
+    # k넘긴사람 저장
+    rp = []
+    for i in range(len(id_list)):
+        if len(r_num[i]) >= k:
+            rp.append(id_list[i])
+
+    answer = []
+    return answer
+
