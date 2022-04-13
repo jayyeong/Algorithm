1289755c2f57b82e4701d5f4de9d504b3ffba744
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-04-14 01:21:44 +0900

    .

--- programmers/신고 결과 받기.py
+++ programmers/신고 결과 받기.py
@@ -12,7 +12,7 @@ def solution(id_list, report, k):
     for i in range(len(id_list)):
         if len(r_num[i]) >= k:
             rp.append(id_list[i])
-    #print(rp) sszz
+    #print(rp) sszzq
 
     reset = set(report)
     #print(reset)
