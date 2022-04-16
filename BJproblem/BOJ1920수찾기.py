9afddac6350aeddcdacca4692e44959b38790b34
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-04-17 02:52:59 +0900

    .

--- BJproblem/BOJ1920수찾기.py
+++ BJproblem/BOJ1920수찾기.py
@@ -1,17 +1,24 @@
+def Bsearch(arr,v,low,high):
+    if low > high:
+        return 0
+    mid = (low+high) // 2
+    if  arr[mid] > v:
+        return Bsearch(arr,v,low,mid - 1)
+    elif arr[mid] < v:
+        return Bsearch(arr,v,mid + 1,high)
+    else:
+        return 1
+
+
 N = int(input())
 A = [int(x) for x in input().split()]
 M = int(input())
 B = [int(x) for x in input().split()]
 
 A.sort() #오름차순으로 정렬
-print(A)
+#print(A)
 for b in B:
-    if b in A:
-        print(1)
-    else:
-        print(0)
+    print(Bsearch(A,b,0,len(A) - 1))
 
-def Bsearch(arr,v,low,high):
-    if low > high:
-        return False
-    mid = (low+high)/2
+
+
