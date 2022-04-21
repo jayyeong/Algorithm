00b174fa9a1ac5760a8a8aac4251df025a632e5c
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-04-22 00:47:14 +0900

    .

--- BJproblem/BOJ1966프린터큐.py
+++ BJproblem/BOJ1966프린터큐.py
@@ -4,8 +4,7 @@ for _ in T:
     N, M = map(int,input().split())
     arr = [int(x) for x in input().split()]
     count = 0
-    while max(arr) != arr[0]:
-        #qqaaaa
+    #while max(arr) != arr[0]:
 
 
 
