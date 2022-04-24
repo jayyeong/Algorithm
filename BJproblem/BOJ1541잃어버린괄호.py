4dff350b6abf12e2a29b950ceb77e6b77f2ae38f
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-04-24 23:39:49 +0900

    .

---
+++ BJproblem/BOJ1541잃어버린괄호.py
@@ -0,0 +1,16 @@
+s = input()
+#print(s)
+a = s.split(sep='-')
+#print(a_int)
+arr = []
+for q in a:
+    bb = q.split(sep='+')
+    n = 0
+    for b in list(map(int,bb)):
+        n += b
+    arr.append(n)
+#print(arr)
+result = arr[0]
+for i in range(1,len(arr)):
+    result -= arr[i]
+print(result)
