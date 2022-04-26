826c12da438d3b4370a3abad25c785b46ac88f87
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-04-26 17:41:27 +0900

    .

--- BJproblem/BOJ5430AC.py
+++ BJproblem/BOJ5430AC.py
@@ -1,8 +1,39 @@
+from collections import deque
+
+import sys
+input = sys.stdin.readline
+
 T = int(input())
 
 for _ in range(T):
-    p = input()
+    p = list(input())
+    del p[-1]
     n = int(input())
-    a = input()
-    aa = list(map(int,a[1:-1].split(sep=',')))
-    print(aa)
+    a = input().strip()
+    if n != 0:
+        de = deque(map(int,a[1:-1].split(sep=',')))
+    else:
+        de = deque()
+    #print(p,aa)
+    Reverseflag = False
+    errorflag = False
+    for s in p:
+        if s == 'R':
+            Reverseflag = not Reverseflag
+
+        if s == 'D':
+            if de:
+                if Reverseflag:
+                    de.pop()
+                else:
+                    de.popleft()
+
+            else:
+                errorflag = True
+                break
+    if errorflag:
+        print("error")
+    else:
+        if Reverseflag:
+            de.reverse()
+        print("[%s]"%(','.join(map(str,de))))
