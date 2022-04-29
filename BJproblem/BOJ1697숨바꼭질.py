8b09add035b09b0d95322d5d48be2f92d3fee3c0
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-04-29 22:28:07 +0900

    .

--- BJproblem/BOJ1697숨바꼭질.py
+++ BJproblem/BOJ1697숨바꼭질.py
@@ -1,4 +1,30 @@
-N, K = map(int,input.split())
-count = 0
+from collections import deque
 
+def bfs():
+    graph = deque()
+    graph.append([N,0])
+    visited = list()
+
+    while graph:
+
+        node = graph[0][0]
+        depth = graph[0][1]
+        graph.popleft()
+
+        if node == K:
+            return depth
+
+        if node - 1 >= 0 and node - 1 <= 100000 and node - 1 not in visited:
+            visited.append(node - 1)
+            graph.append([node - 1,depth + 1])
+        if node + 1 >= 0 and node + 1 <= 100000 and node + 1 not in visited:
+            visited.append(node + 1)
+            graph.append([node + 1,depth + 1])
+        if node * 2 >= 0 and node * 2 <= 100000 and node * 2 not in visited:
+            visited.append(node * 2)
+            graph.append([node * 2,depth + 1])
+
+
+N, K = map(int,input().split())
+print(bfs())
 
