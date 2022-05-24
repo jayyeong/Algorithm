c9331167b5f58ff1c1096c40828f111f73066c2b
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-05-24 21:20:58 +0900

    .

--- BJproblem_May/BOJ2559수열.py
+++ BJproblem_May/BOJ2559수열.py
@@ -1,3 +1,13 @@
 N, K = map(int,input().split())
 arr = [int(x) for x in input().split()]
 
+DP = [0]
+max_value = -1000000
+for i in range(N):
+    DP.append(DP[i] + arr[i])
+#print(DP)
+
+for i in range(N - K + 1):
+    if max_value < DP[i + K] - DP[i]:
+        max_value = DP[i + K] - DP[i]
+print(max_value)
