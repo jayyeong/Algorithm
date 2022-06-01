56731b65b91416620f7191e20beb699adc12c641
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-06-02 01:00:15 +0900

    .

--- BJproblem_May/BOJ2477참외밭.py
+++ BJproblem_May/BOJ2477참외밭.py
@@ -1,10 +1,45 @@
 K = int(input())
 
-Arr = []
+side = []
+length = []
+edge = [[] for _ in range(5)]
+
 for _ in range(6):
     x, y = map(int,input().split())
-    Arr.append([x,y])
-# print(Arr)
-# 동 1, 서 2, 남 3, 북 4
+    side.append(x)
+    length.append(y)
+    edge[x].append(y)
 
+if len(edge[2]) == 1 and len(edge[4]) == 1: # r
+    Widx = side.index(4)
+    Hidx = side.index(2)
+    W = length[Widx]
+    H = length[Hidx]
+    sW = abs(length[(Widx - 1) % 6] - length[(Widx + 1) % 6])
+    sH = abs(length[(Hidx - 1) % 6] - length[(Hidx + 1) % 6])
+    print((W*H - sW*sH)*K)
+if len(edge[2]) == 1 and len(edge[3]) == 1: # ㄴ
+    Widx = side.index(2)
+    Hidx = side.index(3)
+    W = length[Widx]
+    H = length[Hidx]
+    sW = abs(length[(Widx - 1) % 6] - length[(Widx + 1) % 6])
+    sH = abs(length[(Hidx - 1) % 6] - length[(Hidx + 1) % 6])
+    print((W*H - sW*sH)*K)
+if len(edge[3]) == 1 and len(edge[1]) == 1: # ㅢ
+    Widx = side.index(3)
+    Hidx = side.index(1)
+    W = length[Widx]
+    H = length[Hidx]
+    sW = abs(length[(Widx - 1) % 6] - length[(Widx + 1) % 6])
+    sH = abs(length[(Hidx - 1) % 6] - length[(Hidx + 1) % 6])
+    print((W*H - sW*sH)*K)
+if len(edge[1]) == 1 and len(edge[4]) == 1: # ㄱ
+    Widx = side.index(1)
+    Hidx = side.index(4)
+    W = length[Widx]
+    H = length[Hidx]
+    sW = abs(length[(Widx - 1) % 6] - length[(Widx + 1) % 6])
+    sH = abs(length[(Hidx - 1) % 6] - length[(Hidx + 1) % 6])
+    print((W*H - sW*sH)*K)
 
