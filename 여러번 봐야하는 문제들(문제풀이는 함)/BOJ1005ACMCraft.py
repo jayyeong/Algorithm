ba4ae30605a8e6e310cb238978c953481c988b80
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-06-10 01:46:58 +0900

    위상정렬,dp

---
+++ 여러번 봐야하는 문제들(문제풀이는 함)/BOJ1005ACMCraft.py
@@ -0,0 +1,46 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+T = int(input().rstrip())
+
+for _ in range(T):
+    N, K = map(int,input().rstrip().split())
+
+    D = [0]
+    D.extend([int(x) for x in input().rstrip().split()]) # n번째 건물 건설 시간
+    in_degree = [0 for _ in range(N + 1)] # n번째 node 진입 차수
+    adj_list_out = [[] for i in range(N + 1)] # n번째 노드에서 out인 노드 저장
+    adj_list_in = [[] for i in range(N + 1)]  # n번째 노드에서 in인 노드 저장
+    deq = deque()
+    toposort = []
+    Dtime = [0 for i in range(N + 1)] # n번째 건설까지 소요되는 시간
+
+    for i in range(K):
+        x, y = map(int,input().rstrip().split())
+        adj_list_out[x].append(y)
+        adj_list_in[y].append(x)
+        in_degree[y] += 1
+    W = int(input())
+
+    for i in range(1,N + 1):
+        if in_degree[i] == 0:
+            deq.append(i)
+
+    while deq:
+        node = deq.popleft()
+        toposort.append(node)
+        for s in adj_list_out[node]:
+            in_degree[s] -= 1
+            if in_degree[s] == 0:
+                deq.append(s)
+
+    for i in toposort:
+        count = D[i]
+        al = 0
+        for b in adj_list_in[i]:
+            if al < Dtime[b]:
+                al = Dtime[b]
+        Dtime[i] = count + al
+
+    print(Dtime[W])
