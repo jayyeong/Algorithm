69c5bab224fe7dc38826bae89ff911ee8c664409
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-06-11 02:41:38 +0900

    .

--- memomo.py
+++ memomo.py
@@ -1,12 +1,37 @@
-Arr = [2,3]
+N = int(input())
 
-def test(arr: list):
-    box = arr.copy()
-    if box[- 1] == 1:
-        return box
-    box.append(1)
-    return test(box)
+def flip(x,y,arr):
+    k = y + 1 - x
+    for i in range(k//2):
+        temp = -arr[x - 1 + i]
+        arr[x - 1 + i] = -arr[y - 1 - i]
+        arr[y - 1 - i] = temp
+#normal = [int(x) for x in range(1,N + 1)]
 
-print(test(Arr))
-print(Arr)
+def one_check(arr):
+    minuslist = []
+    straight = True
 
+    for a in arr:
+        if a < 0:
+            minuslist.append(a)
+
+    if len(minuslist) == 1:
+        return True
+
+
+    for i in range(1,len(minuslist)):
+        if minuslist[i] - minuslist[i - 1] != 1:
+            straight = False
+
+    if straight:
+        return True
+    return False
+
+for _ in range(5):
+    Fish = [int(x) for x in input().split()]
+    if one_check(Fish):
+        print('one')
+        pass
+    else:
+        print('noone')
--- recuteset.py
+++
@@ -1,6 +0,0 @@
-def re(n):
-    if n < 1:
-        return 2
-    return (2 * re(n - 1)) + 1
-
-print(re(5))
