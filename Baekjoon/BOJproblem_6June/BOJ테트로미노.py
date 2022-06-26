28235322e1b44f0c428036a7f9ed87a2300064f5
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-06-26 14:26:47 +0900

    dfs

--- Baekjoon/BOJproblem_6June/BOJ테트로미노.py
+++ Baekjoon/BOJproblem_6June/BOJ테트로미노.py
@@ -3,6 +3,8 @@ N, M = map(int,input().split())
 input = sys.stdin.readline
 dx = [-1, 1, 0, 0] # 북, 남, 서, 동
 dy = [0, 0, -1, 1]
+paper = []
+ans = 0
 
 #   @@@@   @@
 #          @@
@@ -10,21 +12,16 @@ dy = [0, 0, -1, 1]
 #   @  @@  @@@
 #   @@  @   @
 
-paper = []
-
-
 for i in range(N):
     paper.append([int(x) for x in input().split()])
 
 visited = [[0 for _ in range(M)] for _ in range(N)]
 
-maxi = 0
 def dfs(x,y,n,c):
-    global maxi
+    global dfs_max
     if c == 4:
-        if maxi < n:
-            maxi = n
-
+        if dfs_max < n:
+            dfs_max = n
     else:
         for i in range(4):
             nx = x + dx[i]
@@ -35,49 +32,40 @@ def dfs(x,y,n,c):
                     dfs(nx,ny,n + paper[nx][ny],c + 1)
                     visited[nx][ny] = 0
 
-def t_spin(x,y,n):
-    mm = 0
+def t_spin(x,y):
+    t_max = 0
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < N and 0 <= ny < M:
+        if 0 <= nx < N and 0 <= ny < M: # +1칸
             nnx = nx + dx[i]
             nny = ny + dy[i]
-            if 0 <= nnx < N and 0 <= nny < M:
-                    for h in [-1,1]:
-                        s = n
-                        if i == 0 or i == 1:
+            if 0 <= nnx < N and 0 <= nny < M: # +2칸
+                    for h in [-1,1]: #볼록한 부분
+                        value = paper[x][y]
+                        if i == 0 or i == 1: #세로로 길면
                             sny = ny + h
                             if 0 <= sny < M:
-                                s += paper[nx][ny]
-                                s += paper[nnx][nny]
-                                s += paper[nx][sny]
-                        elif i == 2 or i == 3:
+                                value += paper[nx][ny]
+                                value += paper[nnx][nny]
+                                value += paper[nx][sny]
+                        elif i == 2 or i == 3: #가로로 길면
                             snx = nx + h
                             if 0 <= snx < N:
-                                s += paper[nx][ny]
-                                s += paper[nnx][nny]
-                                s += paper[snx][ny]
-                        if mm < s:
-                            mm = s
-    return mm
+                                value += paper[nx][ny]
+                                value += paper[nnx][nny]
+                                value += paper[snx][ny]
+                        if t_max < value:
+                            t_max = value
+    return t_max
 
-
-#dfs(0,0,paper[0][0],1)
-#print(maxi)
-#print(visited)
-
-result = 0
 for i in range(N):
     for j in range(M):
-        maxi = 0
+        dfs_max = 0
         visited[i][j] = 1
         dfs(i,j,paper[i][j],1)
         visited[i][j] = 0
-        ma = max(maxi,t_spin(i,j,paper[i][j]))
-        if result < ma:
-            result = ma
+        block_max = max(dfs_max, t_spin(i, j))
+        ans = max(ans,block_max)
 
-print(result)
-#각 자리에서 나올수 있는 최대값을 뽑기
-
+print(ans)
