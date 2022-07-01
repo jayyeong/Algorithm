c22bcfb03420fcef7b2c7517db716220e79cfa67
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-07-01 16:28:16 +0900

    문자열

--- 아직 못 푼 문제/BOJ5525IOIOI.py
+++ 아직 못 푼 문제/BOJ5525IOIOI.py
@@ -2,21 +2,10 @@ N = int(input())
 M = int(input())
 S = input()
 
-# P_n = 'I'
-# for i in range(N):
-#     P_n += 'OI'
-# count = 0
-#
-# for i in range(len(S) - len(P_n)):
-#     # print(S[i:i + len(P_n)])
-#     if P_n == S[i:i + len(P_n)]:
-#
-#         count += 1
-
 cursor, count, result = 0, 0, 0
+
 while cursor < (M - 1):
-    #print(S[cursor:cursor + 3])
-    if S[cursor:cursor + 3] == 'IOI': #3칸짜리
+    if S[cursor:cursor + 3] == 'IOI': #3칸
         count += 1
         cursor += 2
         if count == N:
