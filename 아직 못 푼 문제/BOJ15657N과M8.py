6ca628d971d302c17e1143a521b12347658d06ae
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-07-13 19:11:58 +0900

    backtracking

---
+++ 아직 못 푼 문제/BOJ15657N과M8.py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split())
+numbers = [int(x) for x in input().split()]
+
+numbers.sort()
+
+def backtracking(depth):
+    if depth == M:
+        print(' '.join(map(str,box)))
+        return
+
+    for i in range(N):
+        if depth == 0 or box[depth - 1] <= numbers[i]:
+            box.append(numbers[i])
+            backtracking(depth + 1)
+            box.pop()
+
+box = []
+backtracking(0)
