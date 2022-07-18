e89ff5a461bff5637d501fd50cd6422e321028ae
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-07-19 04:21:20 +0900

    Problem Solving

--- 아직 못 푼 문제/BOJ1504특정한최단경로.py
+++ 아직 못 푼 문제/BOJ1504특정한최단경로.py
@@ -1,6 +1,57 @@
-
-
+import sys, heapq
 INF = int(1e9)
+
+N, E = map(int,input().split())
+
+graph = [[] for _ in range(N + 1)]
+
 for _ in range(E):
-    u, v, w = map(int, input().split())
-    graph[u].append((v,w))
+    a, b, c = map(int,sys.stdin.readline().rstrip().split())
+
+    graph[a].append([b, c])
+    graph[b].append([a, c])
+
+v1, v2 = map(int,input().split())
+
+#print(graph)
+
+def dijkstra(start):
+    que = []
+    heapq.heappush(que, (0, start))
+    D[start] = 0
+
+    while que:
+        dist, now = heapq.heappop(que)
+        if D[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < D[i[0]]:
+                D[i[0]] = cost
+                heapq.heappush(que, (cost,i[0]))
+
+
+root_A, root_B = 0, 0
+D = [INF] * (N + 1)
+dijkstra(1)
+
+root_A += D[v1]
+root_B += D[v2]
+
+D = [INF] * (N + 1)
+dijkstra(v1)
+root_A += D[v2]
+root_B += D[N]
+
+D = [INF] * (N + 1)
+dijkstra(v2)
+root_A += D[N]
+root_B += D[v1]
+
+ans = min(root_A, root_B)
+
+if ans >= INF:
+    print(-1)
+else:
+    print(ans)
