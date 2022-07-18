79a6ffdf46bc24a1b6487760558c82f57e62834e
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-07-19 00:22:08 +0900

    Problem Solving

--- 아직 못 푼 문제/BOJ1504특정한최단경로.py
+++ 아직 못 푼 문제/BOJ1504특정한최단경로.py
@@ -1,26 +1,6 @@
-import sys
-N, E = map(int,input().split())
+
+
 INF = int(1e9)
-
-graph = [[INF] * (N + 1) for i in range(N + 1)]
-for i in range(1, N + 1):
-    graph[i][i] = 0
-
 for _ in range(E):
-    a, b, c = map(int, sys.stdin.readline().rstrip().split())
-    graph[a][b] = c
-    graph[b][a] = c
-
-v1, v2 = map(int, input().split())
-
-for i in graph:
-    print(i)
-
-start = 1
-end = N
-
-def dfs(n):
-
-for i in graph:
-    print(i)
-
+    u, v, w = map(int, input().split())
+    graph[u].append((v,w))
