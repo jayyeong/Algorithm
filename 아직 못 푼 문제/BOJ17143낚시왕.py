281000ee56f302d952ccbab62a2431f533a4f9ad
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-08-13 21:49:31 +0900

    Problem Solving

--- 아직 못 푼 문제/BOJ17143낚시왕.py
+++ 아직 못 푼 문제/BOJ17143낚시왕.py
@@ -6,15 +6,14 @@ dy = [0, 0, 1, -1]
 shark_list = []
 ans = 0
 
-for _ in range(M):
+for i in range(M):
     shark_list.append([int(x) for x in sys.stdin.readline().rstrip().split()] + [1])
+    shark_list[i][3] -= 1
     # 위치 (row, col), 속력, 방향, 크기, 생존 유무 1 = 생, 0 = 죽
     # 1 위, 2 아래, 3 오른쪽, 4 왼쪽
 
 def shark_moving(shark):
     r, c, s, d, z, live = shark
-
-    d -= 1
 
     while s > 0:
         nx, ny = r + dx[d], c + dy[d]
@@ -34,38 +33,37 @@ def shark_moving(shark):
                 d = 2
             continue
 
-    return (r, c)
+    return (r, c, d)
 
-
-
-
-
-for i in range(1, C + 1): # 낚시왕이 오른쪽으로 한칸 이동
+for step in range(1, C + 1): # 낚시왕이 오른쪽으로 한칸 이동
 
     nearest_shark = int(1e9)
 
     for s in shark_list:
-        if s[1] == i and s[5] == 1:
+        if s[1] == step and s[5] == 1:
             if nearest_shark > s[0]:
                 nearest_shark = s[0]
 
-    for s in shark_list: # 상어 잡기
-        if s[0] == nearest_shark and s[5] == 1:
-            s[5] = 0
-            ans += s[4]
+    for i in range(len(shark_list)): # 상어 잡기
+        if shark_list[i][0] == nearest_shark and shark_list[i][5] == 1:
+            ans += shark_list[i][4]
+            del shark_list[i]
             break
 
-    print(ans)
+    #print(ans)
 
     for i in range(len(shark_list)):
         if shark_list[i][5] == 1:
-            x, y = shark_moving(shark_list[i])
+            x, y, direc = shark_moving(shark_list[i])
             shark_list[i][0] = x
             shark_list[i][1] = y
+            shark_list[i][3] = direc
 
-    fishing_zone = [[] * (C + 1) for _ in range(R + 1)]
+    print(shark_list)
 
-    print(fishing_zone)
+    fishing_zone = [[[] for _ in range(C + 1)] for _ in range(R + 1)]
+
+    #print(fishing_zone)
     for s in shark_list:
         if s[5] == 1:
             fishing_zone[s[0]][s[1]].append(s)
