7646b9aa30192ec67cdcbf37875646f1b423f529
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-08-16 04:47:55 +0900

    create file

--- 아직 못 푼 문제/BOJ23289온풍기안녕.py
+++ 아직 못 푼 문제/BOJ23289온풍기안녕.py
@@ -2,6 +2,55 @@ import sys
 R, C, K = map(int, input().split())
 
 graph = []
-for _ in range(R):
-    graph.append(list(map(int, sys.stdin.readline().rstrip().split())))
+checking_area = []
+heater = []
 
+for i in range(R):
+    graph.append(list(map(int, sys.stdin.readline().rstrip().split())))
+    for j in range(C):
+        if 0 < graph[i][j] < 5:
+            heater.append([graph[i][j], i, j])
+            graph[i][j] = 0
+
+        elif graph[i][j] == 5:
+            checking_area.append([i, j])
+            graph[i][j] = 0
+
+
+W = int(input())
+
+wall_list = []
+
+for _ in range(W):
+    wall_list.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
+def wind():
+    pass
+
+def adjust():
+    pass
+
+def reduction():
+    pass
+
+def condition_check():
+    for a in checking_area:
+        x, y = a
+        if graph[x][y] < K:
+            return True
+    return False
+
+ans = 0
+
+while True:
+
+    wind()
+    adjust()
+    reduction()
+
+    ans += 1
+
+    if condition_check() == False:
+        break
+
+print(ans)
