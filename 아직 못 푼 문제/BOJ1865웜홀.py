ebd5da667402829129e37ff0dbdab225ef7e38de
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-08-20 01:58:02 +0900

    Problem Solving

---
+++ 아직 못 푼 문제/BOJ1865웜홀.py
@@ -0,0 +1,18 @@
+import sys
+TC = int(input())
+
+for _ in range(TC):
+    N, M, W = map(int, sys.stdin.readline().split())
+
+    roads = []
+    wormholes = []
+
+    for _ in range(M):
+        roads.append([int(x) for x in sys.stdin.readline().split()])
+
+    for _ in range(W):
+        wormholes.append([int(x) for x in sys.stdin.readline().split()])
+
+    print(roads)
+    print(wormholes)
+
