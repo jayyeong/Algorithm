1f97cbf0be91d739e8634bdc0210d0aec08c7af8
jayyeong <48705640+jayyeong@users.noreply.github.com>
2022-11-16 15:20:00 +0900

    Problem Solving

--- 아직 못 푼 문제/17404RGB거리2.py
+++ 아직 못 푼 문제/17404RGB거리2.py
@@ -5,11 +5,18 @@ for _ in range(N):
     rgb.append(list(map(int, sys.stdin.readline().split())))
 
 dp = [[0] * 3 for _ in range(N + 1)]
-dp[0] = rgb[0]
+min_list = []
+for k in range(3):
 
-for i in range(1, N - 1):
-    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0]
-    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1]
-    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2]
+    dp[0] = [1000 * 1000] * 3
+    dp[0][k] = rgb[0][k]
 
-print(min(dp[N - 1]))
+    for i in range(1, N):
+        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0]
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1]
+        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2]
+
+    dp[N - 1][k] = 1000 * 1000
+    min_list.extend(dp[N - 1])
+
+print(min(min_list))
